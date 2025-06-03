81a0a8c799acccfaec1af429d5fe470fb5c4b18b
CordobaIsmael <143009067+CordobaIsmael@users.noreply.github.com>
2025-06-03 15:00:59 -0300

    mandale mecha
    mandale mecha

--- src/routes/pacientes.route.js
+++ src/routes/pacientes.route.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const pacientesController = require('../controllers/API/pacientes.controller.js');
 const  {verifyTokenMiddleware}  = require('../middlewares/verifyToken.middleware.js');
+const {pacienteSchema} = require('../schemes/pacientes.schema.js');
 const rutaPacientes = Router();
 rutaPacientes.get('/', verifyTokenMiddleware, pacientesController.list);
 rutaPacientes.post('/login',pacientesController.login)
-rutaPacientes.post('/',verifyTokenMiddleware,pacientesController.create);
+rutaPacientes.post('/',verifyTokenMiddleware,pacienteSchema.create,pacientesController.create);
 rutaPacientes.put('/:id',verifyTokenMiddleware,pacientesController.update);
 rutaPacientes.delete('/:id',verifyTokenMiddleware,pacientesController.delete);
 
